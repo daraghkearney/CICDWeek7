96e46400b677116cd388fc6c0935501c8861b6b8
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-11-05 14:17:58 +0000

    Project Setup + Configured H2 and JPA

---
+++ src/test/java/ie/atu/cicdweek7/CicdWeek7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdweek7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdWeek7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
